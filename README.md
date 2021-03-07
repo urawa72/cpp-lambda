9e3b444e6f6f75f9c63a3aa1046045a7c24f1cd2
urawa72 <urawa72@gmail.com>
2021-03-07 22:51:14 +0900

    add dynamodb_test

--- README.md
+++ README.md
@@ -9,6 +9,32 @@ docker build -t cpp-lambda .
 ## Build C++ code & create zip file
 
 ```
-# build sample codes
 docker run --rm -v $(pwd)/sample:/src cpp-lambda aws-lambda-package-sample
+```
+
+## Build C++ code at local.
+
+Build aws-sdk-cpp into specified directory
+```
+git clone
+cd aws-sdk-cpp
+mkdir build
+cd build
+cmake .. -DBUILD_ONLY="dynamodb" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/path/to/sdk-install -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DENABLE_TESTING=OFF
+make -j 4
+make install
+```
+
+Build C++ code
+```
+cd dynamodb_test
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/path/to/sdk-install
+make
+
+# execute
+./create_table
+Table SampleTable created!
+* Closing connection 0
 ```
---
+++ dynamodb_test/create_table.cpp
@@ -0,0 +1,57 @@
+#include <aws/core/Aws.h>
+#include <aws/core/utils/Outcome.h>
+#include <aws/dynamodb/DynamoDBClient.h>
+#include <aws/dynamodb/model/AttributeDefinition.h>
+#include <aws/dynamodb/model/CreateTableRequest.h>
+#include <aws/dynamodb/model/KeySchemaElement.h>
+#include <aws/dynamodb/model/ProvisionedThroughput.h>
+#include <aws/dynamodb/model/ScalarAttributeType.h>
+#include <iostream>
+
+
+// see https://docs.aws.amazon.com/sdk-for-cpp/v1/developer-guide/examples-dynamodb-tables.html
+int main()
+{
+    Aws::SDKOptions options;
+
+    Aws::InitAPI(options);
+    {
+        const Aws::String table("SampleTable");
+        const Aws::String region("ap-northeast-1");
+
+        Aws::Client::ClientConfiguration clientConfig;
+        if (!region.empty())
+            clientConfig.region = region;
+        Aws::DynamoDB::DynamoDBClient dynamoClient(clientConfig);
+
+        Aws::DynamoDB::Model::CreateTableRequest req;
+
+        Aws::DynamoDB::Model::AttributeDefinition haskKey;
+        haskKey.SetAttributeName("Name");
+        haskKey.SetAttributeType(Aws::DynamoDB::Model::ScalarAttributeType::S);
+        req.AddAttributeDefinitions(haskKey);
+
+        Aws::DynamoDB::Model::KeySchemaElement keyscelt;
+        keyscelt.WithAttributeName("Name").WithKeyType(Aws::DynamoDB::Model::KeyType::HASH);
+        req.AddKeySchema(keyscelt);
+
+        Aws::DynamoDB::Model::ProvisionedThroughput thruput;
+        thruput.WithReadCapacityUnits(5).WithWriteCapacityUnits(5);
+        req.SetProvisionedThroughput(thruput);
+
+        req.SetTableName(table);
+
+        const Aws::DynamoDB::Model::CreateTableOutcome& result = dynamoClient.CreateTable(req);
+        if (result.IsSuccess())
+        {
+            std::cout << "Table " << result.GetResult().GetTableDescription().GetTableName() <<
+                " created!" << std::endl;
+        }
+        else
+        {
+            std::cout << "Failed to create table: " << result.GetError().GetMessage();
+        }
+    }
+    Aws::ShutdownAPI(options);
+    return 0;
+}

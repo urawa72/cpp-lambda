634b8dae14d646a9384dce46c397f1b38b883843
urawa72 <urawa72@gmail.com>
2021-04-05 23:43:35 +0900

    update for compile_commands.json

--- dynamodb_test/create_table.cpp
+++ dynamodb_test/create_table.cpp
@@ -8,50 +8,49 @@
 #include <aws/dynamodb/model/ScalarAttributeType.h>
 #include <iostream>
 
+// see
+// https://docs.aws.amazon.com/sdk-for-cpp/v1/developer-guide/examples-dynamodb-tables.html
+int main() {
+  Aws::SDKOptions options;
 
-// see https://docs.aws.amazon.com/sdk-for-cpp/v1/developer-guide/examples-dynamodb-tables.html
-int main()
-{
-    Aws::SDKOptions options;
+  Aws::InitAPI(options);
+  {
+    const Aws::String table("SampleTable");
+    const Aws::String region("ap-northeast-1");
 
-    Aws::InitAPI(options);
-    {
-        const Aws::String table("SampleTable");
-        const Aws::String region("ap-northeast-1");
+    Aws::Client::ClientConfiguration clientConfig;
+    if (!region.empty())
+      clientConfig.region = region;
+    Aws::DynamoDB::DynamoDBClient dynamoClient(clientConfig);
 
-        Aws::Client::ClientConfiguration clientConfig;
-        if (!region.empty())
-            clientConfig.region = region;
-        Aws::DynamoDB::DynamoDBClient dynamoClient(clientConfig);
+    Aws::DynamoDB::Model::CreateTableRequest req;
 
-        Aws::DynamoDB::Model::CreateTableRequest req;
+    Aws::DynamoDB::Model::AttributeDefinition haskKey;
+    haskKey.SetAttributeName("Name");
+    haskKey.SetAttributeType(Aws::DynamoDB::Model::ScalarAttributeType::S);
+    req.AddAttributeDefinitions(haskKey);
 
-        Aws::DynamoDB::Model::AttributeDefinition haskKey;
-        haskKey.SetAttributeName("Name");
-        haskKey.SetAttributeType(Aws::DynamoDB::Model::ScalarAttributeType::S);
-        req.AddAttributeDefinitions(haskKey);
+    Aws::DynamoDB::Model::KeySchemaElement keyscelt;
+    keyscelt.WithAttributeName("Name").WithKeyType(
+        Aws::DynamoDB::Model::KeyType::HASH);
+    req.AddKeySchema(keyscelt);
 
-        Aws::DynamoDB::Model::KeySchemaElement keyscelt;
-        keyscelt.WithAttributeName("Name").WithKeyType(Aws::DynamoDB::Model::KeyType::HASH);
-        req.AddKeySchema(keyscelt);
+    Aws::DynamoDB::Model::ProvisionedThroughput thruput;
+    thruput.WithReadCapacityUnits(5).WithWriteCapacityUnits(5);
+    req.SetProvisionedThroughput(thruput);
 
-        Aws::DynamoDB::Model::ProvisionedThroughput thruput;
-        thruput.WithReadCapacityUnits(5).WithWriteCapacityUnits(5);
-        req.SetProvisionedThroughput(thruput);
+    req.SetTableName(table);
 
-        req.SetTableName(table);
-
-        const Aws::DynamoDB::Model::CreateTableOutcome& result = dynamoClient.CreateTable(req);
-        if (result.IsSuccess())
-        {
-            std::cout << "Table " << result.GetResult().GetTableDescription().GetTableName() <<
-                " created!" << std::endl;
-        }
-        else
-        {
-            std::cout << "Failed to create table: " << result.GetError().GetMessage();
-        }
+    const Aws::DynamoDB::Model::CreateTableOutcome &result =
+        dynamoClient.CreateTable(req);
+    if (result.IsSuccess()) {
+      std::cout << "Table "
+                << result.GetResult().GetTableDescription().GetTableName()
+                << " created!" << std::endl;
+    } else {
+      std::cout << "Failed to create table: " << result.GetError().GetMessage();
     }
-    Aws::ShutdownAPI(options);
-    return 0;
+  }
+  Aws::ShutdownAPI(options);
+  return 0;
 }
---
+++ sns_sample/main.cpp
@@ -0,0 +1,7 @@
+#include <aws/core/Aws.h>
+#include <aws/sns/SNSClient.h>
+#include <aws/sns/model/PublishRequest.h>
+#include <aws/sns/model/PublishResult.h>
+#include <iostream>
+
+int main() { return 0; }

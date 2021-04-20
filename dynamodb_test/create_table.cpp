caddfaabf4663231064860a7949596086eb89d84
urawa72 <urawa72@gmail.com>
2021-04-20 21:08:23 +0900

    update

--- dynamodb_test/create_table.cpp
+++ dynamodb_test/create_table.cpp
@@ -6,22 +6,38 @@
 #include <aws/dynamodb/model/KeySchemaElement.h>
 #include <aws/dynamodb/model/ProvisionedThroughput.h>
 #include <aws/dynamodb/model/ScalarAttributeType.h>
+
 #include <iostream>
 
-// see
-// https://docs.aws.amazon.com/sdk-for-cpp/v1/developer-guide/examples-dynamodb-tables.html
-int main() {
+int main(int argc, char **argv) {
+  const std::string USAGE =
+      "\n"
+      "Usage:\n"
+      "    create_table <table> <optional:region>\n\n"
+      "Where:\n"
+      "    table - the table to create\n"
+      "    region- optional region\n\n"
+      "Example:\n"
+      "    create_table HelloTable us-west-2\n";
+
+  if (argc < 2) {
+    std::cout << USAGE;
+    return 1;
+  }
+
   Aws::SDKOptions options;
 
   Aws::InitAPI(options);
   {
-    const Aws::String table("SampleTable");
-    const Aws::String region("ap-northeast-1");
+    const Aws::String table(argv[1]);
+    const Aws::String region(argc > 2 ? argv[2] : "");
 
     Aws::Client::ClientConfiguration clientConfig;
-    if (!region.empty())
-      clientConfig.region = region;
+    if (!region.empty()) clientConfig.region = region;
     Aws::DynamoDB::DynamoDBClient dynamoClient(clientConfig);
+
+    std::cout << "Creating table " << table
+              << " with a simple primary key: \"Name\"" << std::endl;
 
     Aws::DynamoDB::Model::CreateTableRequest req;
 
@@ -44,7 +60,7 @@ int main() {
     const Aws::DynamoDB::Model::CreateTableOutcome &result =
         dynamoClient.CreateTable(req);
     if (result.IsSuccess()) {
-      std::cout << "Table "
+      std::cout << "Table \""
                 << result.GetResult().GetTableDescription().GetTableName()
                 << " created!" << std::endl;
     } else {
